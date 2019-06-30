12935a84db9479fa904c0e0b69df989396e2162c
QPotato <federicobadaloni@hotmail.com>
2019-06-30 16:31:04 -0300

    test

--- src/test/test_parser.rs
+++ src/test/test_parser.rs
@@ -1,8 +1,26 @@
-use ast::lexer;
-use ast::parser;
+use std::fs::{read_dir, read_to_string};
+
+use ast::parser::{parse, ParseError};
 
 
 #[test]
-fn test_lexer() {
-    assert!(1 == 1);
+fn test_good() {
+    let good_path = "./tiger_sources/good/";
+    let source_files = read_dir(good_path).unwrap();
+    for direntry in source_files {
+        let path = direntry.unwrap().path();
+        let mut contents = read_to_string(&path).unwrap();
+        parse(contents).expect("Compilation filed");
+    }
+}
+
+#[test]
+fn test_syntax() {
+    let syntax_path =  "./tiger_sources/syntax/";
+    let source_files = read_dir(syntax_path).unwrap();
+    for direntry in source_files {
+        let path = direntry.unwrap().path();
+        let mut contents = read_to_string(&path).unwrap();
+        assert_eq!(parse(contents), Err(ParseError));
+    }
 }
