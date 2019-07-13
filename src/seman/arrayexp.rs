2a677996cf56f2d6ef5c3f2fa23faae620ee3819
QPotato <federicobadaloni@hotmail.com>
2019-07-13 18:58:14 -0300

    Tipado ArrayExp

--- src/seman/arrayexp.rs
+++ src/seman/arrayexp.rs
@@ -18,7 +18,29 @@ pub fn tipar(exp: Exp, type_env: TypeEnviroment, value_env: ValueEnviroment) -> 
     //     },
     //     _ => panic!("mal delegado")
     // }
-    return Ok(Tipo::TUnit);
+    use _Exp::*;
+    use Var::*;
+    use Tipo::*;
+    match exp {
+        Exp { node: ArrayExp { typ: array_of_symbol, size: size_exp, init: init_exp}, pos}
+            => match type_env.get(&array_of_symbol) {
+                Some(TArray(array_of_type, type_id)) => match tipar_exp(*size_exp, type_env.clone(), value_env.clone()) {
+                    Ok(TInt(_)) => match tipar_exp(*init_exp, type_env.clone(), value_env.clone()) {
+                        Ok(init_type) => if **array_of_type == init_type {
+                            Ok(TArray(Box::new(*array_of_type.clone()), *type_id))
+                        } else {
+                            Err(TypeError::TypeMismatch(pos))
+                        },
+                        Err(e) => Err(e)
+                    },
+                    Ok(_) => Err(TypeError::NonIntegerSize(pos)),
+                    Err(e) => Err(e)
+                },
+                Some(_) => Err(TypeError::NotArrayType(pos)),
+                None => Err(TypeError::UndeclaredType(pos))
+            },
+        _ => panic!("le llego algo nada que ver a arrayexp::tipar")
+    }
 }
 
 pub fn traducir(exp: Exp) -> ExpInterm {
