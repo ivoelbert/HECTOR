c93b85e64cfe76d1c08d73241349f9a64eb5f9c8
fede <federicobadaloni@hotmail.com>
2019-07-18 01:41:02 -0300

    tipoReal function to find the real type of a TTipo

--- src/seman/whileexp.rs
+++ src/seman/whileexp.rs
@@ -6,9 +6,11 @@ pub fn tipar(exp: Exp, type_env: TypeEnviroment, value_env: ValueEnviroment) -> 
         match tipar_exp(*body, type_env.clone(), value_env.clone()) {
             // El ML dice que el body tiene que ser TUnit pero me parece una pavada eso.
             Ok(_) => match tipar_exp(*test, type_env.clone(), value_env.clone()) {
-                Ok(Tipo::TInt(_)) => Ok(Tipo::TUnit),
-                // Aca habria que tener en cuenta el tipoReal
-                Ok(_) => Err(TypeError::NonIntegerCondition(pos)),
+                Ok(test_type) => match tipo_real(test_type, type_env.clone()) {
+                    Some(Tipo::TInt(R::RW)) => Ok(Tipo::TUnit),
+                    Some(_) =>  Err(TypeError::NonIntegerCondition(pos)),
+                    None => panic!("real type panic")
+                },
                 Err(type_error) => Err(type_error)
             },
             Err(type_error) => return Err(type_error)
