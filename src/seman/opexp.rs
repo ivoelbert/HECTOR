0f50f5ae06d3c0f67711a30b8fdac2c8f64a67bb
fede <federicobadaloni@hotmail.com>
2019-07-18 03:19:53 -0300

    All typing tests ok

--- src/seman/opexp.rs
+++ src/seman/opexp.rs
@@ -2,7 +2,21 @@ use super::super::ast::tigerabs::*;
 use super::tigerseman::*;
 
 pub fn tipar(exp: Exp, type_env: TypeEnviroment, value_env: ValueEnviroment) -> Result<Tipo, TypeError> {
-    return Ok(Tipo::TUnit);
+    match exp { Exp {node: _Exp::OpExp{left, oper, right}, pos} => {
+        let _ = match tipar_exp(*left, type_env.clone(), value_env.clone()) {
+            Ok(Tipo::TInt(_)) => (),
+            Ok(_) => return Err(TypeError::NonIntegerOperand(pos)),
+            Err(type_error) => return Err(type_error)
+        };
+        let _ = match tipar_exp(*right, type_env.clone(), value_env.clone()) {
+            Ok(Tipo::TInt(_)) => (),
+            Ok(_) => return Err(TypeError::NonIntegerOperand(pos)),
+            Err(type_error) => return Err(type_error)
+        };
+        Ok(Tipo::TInt(R::RW))
+    }
+    _ => panic!("delegation errror on opexp::tipar")
+    }
 }
 
 pub fn traducir(exp: Exp) -> ExpInterm {
--- src/seman/seqexp.rs
+++ src/seman/seqexp.rs
@@ -2,7 +2,22 @@ use super::super::ast::tigerabs::*;
 use super::tigerseman::*;
 
 pub fn tipar(exp: Exp, type_env: TypeEnviroment, value_env: ValueEnviroment) -> Result<Tipo, TypeError> {
-    return Ok(Tipo::TUnit);
+    use Tipo::*;
+    match exp { Exp {node: _Exp::SeqExp(exps), pos: _} => {
+        let mut seq_type : Tipo = TUnit;
+        if exps.len() == 0 {
+            panic!("0 length SeqExp");
+        }
+        for exp in exps {
+            match tipar_exp(*exp, type_env.clone(), value_env.clone()) {
+                Ok(t) => seq_type = t,
+                Err(type_error) => return Err(type_error)
+            }
+        }
+        Ok(seq_type)
+    }
+    _ => panic!("delegation panic on seqexp::tipar")
+    }
 }
 
 pub fn traducir(exp: Exp) -> ExpInterm {
