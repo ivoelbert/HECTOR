c0fd4217131713621ed67aa4bc6ec0ce0d761d36
QPotato <federicobadaloni@hotmail.com>
2020-01-09 16:46:51 -0300

    changes from rs

--- app/src/Components/Compiler.tsx
+++ app/src/Components/Compiler.tsx
@@ -4,7 +4,7 @@ import { CompilerInterface } from './CompilerInterface';
 const AsyncCompiler = React.lazy(
     async (): Promise<any> => {
         const wasm = await import('tigerust');
-        const Component: React.FC = () => <CompilerInterface compile={wasm.main} />;
+        const Component: React.FC = () => <CompilerInterface compile={wasm.compile} />;
 
         return { default: Component };
     }
