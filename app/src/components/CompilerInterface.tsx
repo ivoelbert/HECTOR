0ec6ef7cc6147e0bb417b40dc9f6eda22b1ca1b1
ivoelbert <ivo.elbert1@gmail.com>
2020-01-13 15:08:44 -0300

    Adapt webapp to support new compile function

--- app/src/components/CompilerInterface.tsx
+++ app/src/components/CompilerInterface.tsx
@@ -10,24 +10,34 @@ const baseCode = `
 `;
 
 interface CompilerProps {
-    compile(source: string): any;
+    compile(
+        source: string
+    ): {
+        parse: any;
+        typecheck: any;
+        escape: any;
+        translate: any;
+        canon: any;
+        wasm: any;
+    };
 }
 
 export const CompilerInterface: React.FC<CompilerProps> = ({ compile }) => {
     const [code, setCode] = useLocalStorageState<string>('hector-code', baseCode);
     const [ast, setAst] = useState({});
-    const [fragments, setFragments] = useState([]);
+    const [fragments, setFragments] = useState({});
 
     const onClick = () => {
-        const [compiledAst, compiledFragments] = compile(code);
-        setAst(compiledAst);
-        setFragments(compiledFragments);
+        const result = compile(code);
+        console.log();
+        setAst(result.escape);
+        setFragments(result.translate);
     };
 
     const tabs = {
         Editor: <CodeEditor code={code} setCode={setCode} />,
         AST: <ASTViewer ast={ast} />,
-        Result: <p>3</p>
+        Result: <p>not implemented</p>,
     };
 
     return (
