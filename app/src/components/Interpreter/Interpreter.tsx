24c5fff0d83f55d5d6802704fe089a38177d9f47
ivoelbert <ivo.elbert1@gmail.com>
2020-04-05 15:14:51 -0300

    Frontend interpreter tab

--- app/src/components/Interpreter/Interpreter.tsx
+++ app/src/components/Interpreter/Interpreter.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { CanonResult } from '../Compiler/CompilerInterface';
-import './Interpreter.scss'
+import './Interpreter.scss';
+
+const noop = () => {};
 
 interface InterpreterProps {
     canon: CanonResult;
 }
 export const Interpreter: React.FC<InterpreterProps> = props => {
+    const fragmentsAvailable = props.canon !== null;
+
     return (
         <div className="interpreter-container">
-            <p>Not implemented... YET!</p>
+            {fragmentsAvailable && <RunButton onClick={noop} />}
         </div>
-    )
+    );
+};
+
+interface RunButtonProps {
+    onClick: () => void;
+}
+
+const RunButton: React.FC<RunButtonProps> = props => {
+    return (
+        <button className="run-button" onClick={props.onClick}>
+            Run interpreter
+        </button>
+    );
 };
