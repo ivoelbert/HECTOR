3e419b803ab20ff09c7b010ef80db30a99635904
ivoelbert <ivo.elbert1@gmail.com>
2020-04-15 20:41:11 -0300

    Added build script and test from tig files

--- app/config-overrides.js
+++ app/config-overrides.js
@@ -5,8 +5,8 @@ module.exports = function override(config, env) {
 
     config.resolve.extensions.push('.wasm');
 
-    config.module.rules.forEach(rule => {
-        (rule.oneOf || []).forEach(oneOf => {
+    config.module.rules.forEach((rule) => {
+        (rule.oneOf || []).forEach((oneOf) => {
             if (oneOf.loader && oneOf.loader.indexOf('file-loader') >= 0) {
                 // Make file-loader ignore WASM files
                 oneOf.exclude.push(wasmExtensionRegExp);
