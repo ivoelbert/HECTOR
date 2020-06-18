c0460fe95470dfd1a95e8738d8be9ca9b8f9cd11
QPotato <federicobadaloni@hotmail.com>
2020-06-18 07:19:38 -0300

    revert recordexp args in memory

--- src/tree/translate/recordexp.rs
+++ src/tree/translate/recordexp.rs
@@ -17,15 +17,15 @@ pub fn trans_exp(
         Exp::Record { fields, .. } => {
             use std::convert::TryInto;
             // Translate field initializations
-            let (mut fields_exps, fields_level, fields_frags) = fields
+            let (mut fields_exps, mut fields_level, fields_frags) = fields
                 .iter()
                 .try_fold((HashMap::new(), level, frags), |(mut exps, level, frags), (name, ast)| {
                     let (exp, level, frags) = super::trans_exp(ast, level, value_env, breaks_stack, frags)?;
                     exps.insert(name.clone(), exp);
                     Ok((exps, level, frags))
             })?;
-            // let memory_address = fields_level.alloc_local(false, None);
-            let runtime_label = if let
+            let memory_address = fields_level.alloc_local(false, None);
+            let label = if let
                 EnvEntry::Func {label, ..} = value_env.get("+alloc_record").expect("should be in initial value env")
                 { label } else {panic!("typechecking should handle this")};
             let formals = if let
@@ -36,7 +36,7 @@ pub fn trans_exp(
                 .map(|(name, _, order)| {
                     Move!(
                         MEM(Box::new(plus!(
-                            GLOBAL(STACK_POINTER.to_string()),
+                            fields_level.access_to_exp(memory_address.clone(), fields_level.nesting_depth),
                             CONST(*order * WORD_SIZE)
                         ))),
                         fields_exps.remove(name).expect("typechecking should handle this")
@@ -44,29 +44,19 @@ pub fn trans_exp(
                 })
                 .collect()
             );
-            // Ok((
-            //     // Alloc the record memory, store memory address in a temp.
-            //     // copy the inits
-            //     // return the temp with the address
-            //     ESEQ(
-            //         Box::new(seq(vec![
-            //             Move!(
-            //                 fields_level.access_to_exp(memory_address.clone(), fields_level.nesting_depth),// This returns the memory address of the malloc result
-            //                 external_call(label.clone(), vec![CONST(fields_exps.len().try_into().unwrap())])
-            //             ),
-            //             init_statement
-            //         ])),
-            //         Box::new(fields_level.access_to_exp(memory_address, fields_level.nesting_depth)),
-            //     ),
-            //     fields_level, fields_frags
-            // ))
             Ok((
-                // Copy inits to the stack
-                // Call runtime
-                // Return the resulting memory address
+                // Alloc the record memory, store memory address in a temp.
+                // copy the inits
+                // return the temp with the address
                 ESEQ(
-                    Box::new(init_statement),
-                    Box::new(external_call(runtime_label.to_string(), vec![CONST(fields_exps.len().try_into().unwrap())]))
+                    Box::new(seq(vec![
+                        Move!(
+                            fields_level.access_to_exp(memory_address.clone(), fields_level.nesting_depth),// This returns the memory address of the malloc result
+                            external_call(label.clone(), vec![CONST(fields_exps.len().try_into().unwrap())])
+                        ),
+                        init_statement
+                    ])),
+                    Box::new(fields_level.access_to_exp(memory_address, fields_level.nesting_depth)),
                 ),
                 fields_level, fields_frags
             ))
